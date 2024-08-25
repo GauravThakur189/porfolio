7f41335ac1bfb1a358ac3d80db7d59e1a424871e
Gaurav Singh <gauravsingh1892000@gmail.com>
2024-08-25 18:07:56 +0530

    navbar section completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
 
 const App = () => {
   return (
+    <>
+    <Navbar/>
+    <Home></Home>
     <div className="font-bold">App</div>
+    </>
   )
 }
 
