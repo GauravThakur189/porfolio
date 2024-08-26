103eed7801bee9b76bd5e9481cf812e8f6357a4a
Gaurav Singh <gauravsingh1892000@gmail.com>
2024-08-26 16:05:31 +0530

    about section

---
+++ src/components/About.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white' >
+        <div className=' max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+              <p className=' text-4xl font-bold inline border-b-4 border-gray-500'>About</p>  
+            </div>
+            <p className=' text-xl mt-20'>
+               Lorem.rfkfnsormrfoinwfkjnsefujkekwefebsb
+               skrgbuiwibfibwibiw
+               aefeonwornsnfkjbwifnkafjkwenef
+               fknfgrkalnoiwjnofekw eibwefk
+               fkjwnrgk jerg v rinfkwfwfjn w kwenksesfniowenfe few kjkwnfoiwnpqo fqko. 
+            </p>
+
+            <br />
+            <p className='text-xl'>
+                kjrsgnienreiol
+                slfrnwionanweionk nio3  wfjk fiw4w wfe 4w4w iwewfj 4f w4 wfj 4jw ewj erjeogw foww  4tw fjw4  wefjl5jer egrjgu46ik ejk ker gjkmt jsffkr gre egr th er georkm g ekr gke kje rrgoerkijegrj.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
